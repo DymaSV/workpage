894b50a78787cedb8539781f931bef494c7fbf0e
Dmytro Sv <dymasvyrydonov@gmail.com>
2020-06-11 09:16:50 -0400

    Created page for uploading two images

--- pages/image.js
+++ pages/image.js
@@ -2,6 +2,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
 class Image extends React.Component {
+    
     render() {
         return <Grid
             container
@@ -9,14 +10,14 @@ class Image extends React.Component {
             justify="center"
             alignItems="center"
             spacing={0}>
-            <Grid item>
-                <Paper elevation={1}>
-                    <img id="myImg" src={this.props.src1} alt={this.props.text1} />
+            <Grid item xs={6}>
+                <Paper elevation={3}>
+                    <img id="myImg" src={this.props.src1} alt="Image #1" width="100%"/>
                 </Paper>
             </Grid>
-            <Grid item>
-                <Paper elevation={1}>
-                    <img id="myImg" src={this.props.src2} alt={this.props.text2} />
+            <Grid item xs={6} spacing={3}>
+                <Paper elevation={3}>
+                    <img id="myImg" src={this.props.src2} alt="Image #2" width="100%"/>
                 </Paper>
             </Grid>
         </Grid>
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,59 @@
-import PathTab from './path-tab'
+import Grid from '@material-ui/core/Grid';
 import Image from './image'
+import Paper from '@material-ui/core/Paper';
 
 export default function Home() {
   return (
-    <CompareImages/>
+    <CompareImages />
   )
 }
 
 class CompareImages extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {src1: '', src2:''};
+    this.state = { src1: '', src2: '' };
+  }
 
-    this.handleSubmit = this.handleSubmit.bind(this);
+  handleFirstImageSubmit = (event) => {
+    let file = event.target.files[0]
+    var reader = new window.FileReader();
+    if (file) {
+      var url = reader.readAsDataURL(file);
+    }
+    console.log(reader)
+    reader.onloadend = function (e) {
+      this.setState({ src1: [reader.result] });
+    }.bind(this);
   }
-  handleSubmit(event) {
-    this.setState({src1: event.src1, src2: event.src2});
-    console.log(event.src2)
+
+  handleSecondImageSubmit = (event) => {
+    let file = event.target.files[0]
+    var reader = new window.FileReader();
+    if (file) {
+      var url = reader.readAsDataURL(file);
+    }
+    console.log(reader)
+    reader.onloadend = function (e) {
+      this.setState({ src2: [reader.result] });
+    }.bind(this);
   }
+
   render() {
     return <div>
-      <PathTab handleSubmitForm={this.handleSubmit} src1={this.state.src1} src2={this.state.src2}/>
-      <Image src1={this.state.src1} src2={this.state.src2}/>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={0}>
+        <Paper elevation={1}>
+          <input type="file" onChange={this.handleFirstImageSubmit} />
+        </Paper>
+        <Paper elevation={1}>
+          <input type="file" onChange={this.handleSecondImageSubmit} />
+        </Paper>
+      </Grid>
+      <Image src1={this.state.src1} src2={this.state.src2} />
     </div >
   }
 }
